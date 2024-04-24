24dc72598b4d6bc034469fd60545dcf7f34a08be
AbhishekAbhiMS <128393773+AbhishekAbhiMS@users.noreply.github.com>
2024-04-24 12:57:37 +0530

    new files are added
    new project files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Typography } from '@mui/material';
+import logo from './logo.svg';
+import Paymentpage from './Paymentpage/Paymentpage';
+import Preview from './Preview/Preview';
+import Planview from './Planview/Planview';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Paymentpage></Paymentpage> */}
+      {/* <Preview></Preview> */}
+      <Planview></Planview>
+ </div>
+  );
+}
+
+export default App;
